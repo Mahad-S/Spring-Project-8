db1115c53719862ffd59890bb1c1e3cc1b302e90
Mahad <ac75239@skola.goteborg.se>
2026-05-05 11:11:06 +0200

    First commit

---
+++ bookingsystem/src/main/java/se/gruppprojekt/App.java
@@ -0,0 +1,13 @@
+package se.gruppprojekt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
